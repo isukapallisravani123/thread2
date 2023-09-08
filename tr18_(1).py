91d56118619d9058821baa8ee35cabb3eacb87de
isukapallisravani123 <137806436+isukapallisravani123@users.noreply.github.com>
2023-09-08 19:59:48 +0530

    Update tr18_(1).py

--- tr18_(1).py
+++ tr18_(1).py
@@ -48,7 +48,7 @@ def lemmatizer(text):
     return ' '.join(sent)
 
 st.title("Sentiment Analysis App")
-st.markdown("By Nirmal Gaud")
+st.markdown("By Sravani")
 image = Image.open("instathreadpic.jpeg")
 st.image(image, use_column_width=True)
 
